9caa10b08d65f7e66162a38f4c95198f92f4a7c9
Donistr <deniselag@mail.ru>
2024-07-27 00:08:17 +0400

    test: сделал тесты для мапперов

---
+++ src/test/java/org/example/deal/mapper/ContractorMapperTest.java
@@ -0,0 +1,90 @@
+package org.example.deal.mapper;
+
+import org.example.deal.dto.ContractorDTO;
+import org.example.deal.dto.ContractorRoleDTO;
+import org.example.deal.entity.Contractor;
+import org.example.deal.entity.ContractorRole;
+import org.example.deal.entity.DealContractorRole;
+import org.example.deal.entity.help.ContractorRoleEnum;
+import org.example.deal.entity.help.DealContractorRoleId;
+import org.example.deal.mapper.impl.ContractorMapperImpl;
+import org.example.deal.mapper.impl.ContractorRoleMapperImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+import java.util.List;
+import java.util.UUID;
+
+@SpringJUnitConfig
+public class ContractorMapperTest {
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public ContractorRoleMapper contractorRoleMapper() {
+            return new ContractorRoleMapperImpl();
+        }
+
+        @Bean
+        public ContractorMapper contractorMapper() {
+            return new ContractorMapperImpl(contractorRoleMapper());
+        }
+    }
+
+    @Autowired
+    private ContractorMapper contractorMapper;
+
+    @Test
+    public void mapContractorToDtoTest() {
+        Contractor contractor = Contractor.builder()
+                .id(UUID.fromString("e669b707-e162-4ae6-8555-c8a68006535e"))
+                .contractorId("asd123")
+                .name("name123")
+                .inn("inn123")
+                .main(false)
+                .roles(List.of(DealContractorRole.builder()
+                        .id(DealContractorRoleId.builder()
+                                .contractorRole(ContractorRole.builder()
+                                        .id(ContractorRoleEnum.BORROWER)
+                                        .build())
+                                .build())
+                        .build()))
+                .build();
+
+        ContractorDTO contractorDTO = contractorMapper.map(contractor);
+
+        Assertions.assertEquals(contractor.getId(), contractorDTO.getId());
+        Assertions.assertEquals(contractor.getContractorId(), contractorDTO.getContractorId());
+        Assertions.assertEquals(contractor.getName(), contractorDTO.getName());
+        Assertions.assertEquals(contractor.getInn(), contractorDTO.getInn());
+        Assertions.assertEquals(contractor.getMain(), contractorDTO.getMain());
+        Assertions.assertEquals(contractor.getRoles().get(0).getId().getContractorRole().getId(), contractorDTO.getRoles().get(0).getId());
+    }
+
+    @Test
+    public void mapDtoToContractorTest() {
+        ContractorDTO contractorDTO = ContractorDTO.builder()
+                .id(UUID.fromString("e669b707-e162-4ae6-8555-c8a68006535e"))
+                .contractorId("asd123")
+                .name("name123")
+                .inn("inn123")
+                .main(false)
+                .roles(List.of(ContractorRoleDTO.builder()
+                        .id(ContractorRoleEnum.BORROWER)
+                        .build()))
+                .build();
+
+        Contractor contractor = contractorMapper.map(contractorDTO);
+
+        Assertions.assertEquals(contractorDTO.getId(), contractor.getId());
+        Assertions.assertEquals(contractor.getContractorId(), contractorDTO.getContractorId());
+        Assertions.assertEquals(contractorDTO.getName(), contractor.getName());
+        Assertions.assertEquals(contractorDTO.getInn(), contractor.getInn());
+        Assertions.assertEquals(contractorDTO.getMain(), contractor.getMain());
+    }
+
+}
---
+++ src/test/java/org/example/deal/mapper/ContractorRoleMapperTest.java
@@ -0,0 +1,58 @@
+package org.example.deal.mapper;
+
+import org.example.deal.dto.ContractorRoleDTO;
+import org.example.deal.entity.ContractorRole;
+import org.example.deal.entity.help.ContractorRoleEnum;
+import org.example.deal.mapper.impl.ContractorRoleMapperImpl;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+
+@SpringJUnitConfig
+public class ContractorRoleMapperTest {
+
+    @TestConfiguration
+    static class TestConfig {
+        @Bean
+        public ContractorRoleMapper contractorRoleMapper() {
+            return new ContractorRoleMapperImpl();
+        }
+    }
+
+    @Autowired
+    private ContractorRoleMapper contractorRoleMapper;
+
+    @Test
+    public void mapContractorRoleToDTOTest() {
+        ContractorRole contractorRole = ContractorRole.builder()
+                .id(ContractorRoleEnum.BORROWER)
+                .name("name")
+                .category("category")
+                .build();
+
+        ContractorRoleDTO contractorRoleDTO = contractorRoleMapper.map(contractorRole);
+
+        Assertions.assertEquals(contractorRole.getId(), contractorRoleDTO.getId());
+        Assertions.assertEquals(contractorRole.getName(), contractorRoleDTO.getName());
+        Assertions.assertEquals(contractorRole.getCategory(), contractorRoleDTO.getCategory());
+    }
+
+    @Test
+    public void mapDTOToContractorRoleTest() {
+        ContractorRoleDTO contractorRoleDTO = ContractorRoleDTO.builder()
+                .id(ContractorRoleEnum.BORROWER)
+                .name("name")
+                .category("category")
+                .build();
+
+        ContractorRole contractorRole = contractorRoleMapper.map(contractorRoleDTO);
+
+        Assertions.assertEquals(contractorRoleDTO.getId(), contractorRole.getId());
+        Assertions.assertEquals(contractorRoleDTO.getName(), contractorRole.getName());
+        Assertions.assertEquals(contractorRoleDTO.getCategory(), contractorRole.getCategory());
+    }
+
+}
